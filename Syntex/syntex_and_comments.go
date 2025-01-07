77a30e881972f0807a0985632cecea1af698b4de
melias198 <melias4600@gmail.com>
2025-01-07 21:35:38 +0600

    first commit

---
+++ Syntex/syntex_and_comments.go
@@ -0,0 +1,10 @@
+package main // In Go, every program is part of a package. We define this using the package keyword.
+
+import "fmt" // import "fmt" lets us import files included in the fmt package.
+
+func main() { // func main() is the entry point of every Go program.
+	fmt.Println("Hello, World!") //  is a function made available from the fmt package. It is used to output/print text.
+}
+
+// This is a single line comment
+/* This is a multi-line comment */
